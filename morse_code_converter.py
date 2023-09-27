521d27138f6fdd9c5b2a093e1060e971a30cfb69
twinkledeath <milenam2608@gmail.com>
2023-09-27 22:17:58 +0200

    committing all files in the project to upload on github

---
+++ morse_code_converter.py
@@ -0,0 +1,39 @@
+"""
+Morse Code Converter
+
+Takes in a message either directly through command line or prompting the user for input
+and converts it into morse code dashes and dots
+"""
+import sys
+import pandas as pd
+
+MORSE_CODE_PATH = "data\\morse.csv"
+
+
+class MorseCodeConverter:
+    def __init__(self):
+        data = pd.read_csv(MORSE_CODE_PATH)
+        self.morse_dict = {row.letter: row.code for (index,row) in data.iterrows()}
+    
+    def convert(self, message):
+        output = None
+        try:
+            morse_code = [self.morse_dict[letter.upper()] for letter in message]
+            output =  " ".join(morse_code)
+            print(f"The morse code for {message} is: ", output)
+        except KeyError:
+            print("Only latin letters and numbers are supported.")
+
+
+
+if __name__ == "__main__":
+    converter = MorseCodeConverter()
+    try:
+        # for reading in message to convert directly from the command line
+        message = sys.argv[1]
+    except IndexError:
+        message = input("Type your message: ")
+    
+    converter.convert(message)
+
+    
